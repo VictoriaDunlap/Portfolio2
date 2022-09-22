92a4f69044f80e2f534a725904541ac0b5d0f856
victoriaDunlap <victoriacdunlap@gmail.com>
2022-09-21 22:37:49 -0400

    fixed icons with fontawesome and added google fonts. also fixed the mailto button

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,20 +16,20 @@ function Footer() {
             <section class="mb-4">
                 
                 <a id="resume-button" class="btn btn-primary btn-floating m-1" target="_blank" rel="noreferrer noopener" href="https://accounts.google.com/signin/v2/identifier?flowName=GlifWebSignIn&flowEntry=ServiceLogin" role="button">
-                    <i class="bi bi-envelope-open-heart-fill"> victoriacdunlap@gmail.com</i>
+                <i class="fa-solid fa-envelope"></i> victoriacdunlap@gmail.com
                 </a>
 
 
-                <a id="resume-button" class="btn btn-primary btn-floating m-1" target="_blank" rel="noreferrer noopener" href="https://github.com/VictoriaDunlap" role="button">
-                    <i class="bi bi-github"> GitHub</i>
+                <a id="resume-button" class="btn btn-primary btn-floating m-1" target="_blank" rel="noreferrer noopener" href="mailto:victoriacdunlap@gmail.com" role="button">
+                <i class="fa-brands fa-github"></i> GitHub
                 </a>
 
                 <a id="resume-button" class="btn btn-primary btn-floating m-1" target="_blank" rel="noreferrer noopener" href="https://www.linkedin.com/in/victoria-dunlap/" role="button">
-                    <i class="bi bi-linkedin"> LinkedIn</i>
+                <i class="fa-brands fa-linkedin"></i> LinkedIn
                 </a>
 
                 <a id="resume-button" class="btn btn-primary btn-floating m-1" target="_blank" rel="noreferrer noopener" href="https://twitter.com/DunlapCharli" role="button">
-                    <i class="bi bi-twitter"> Twitter</i>
+                <i class="fa-brands fa-twitter"></i> Twitter
                 </a>
 
             </section>
